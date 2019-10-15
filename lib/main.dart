8656d0ea90d6125c700c8eccebc9c643199b1368
Dmitry Koltovich <steklopod@gmail.com>
2019-10-15 20:32:39 +0300

    I am rich - v.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('Hello'),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Я богат'),
+          backgroundColor: Colors.orangeAccent,
         ),
+        backgroundColor: Colors.blueGrey.shade200,
+        body: Center(
+            child: Image(
+          image: AssetImage('images/diamond.png'),
+        )),
       ),
-    );
+    ),
+  );
+}
