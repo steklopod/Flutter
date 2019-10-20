d2f67fb209a9b7fce99d1907d094e8c06c7ace93
Dmitry Koltovich <steklopod@gmail.com>
2019-10-20 12:53:07 +0300

    02: practic is done

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,12 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-//            mainAxisSize: MainAxisSize.min,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            mainAxisSize: MainAxisSize.max,
 //            verticalDirection: VerticalDirection.up,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              container,
-              container_2,
-              SizedBox(height: 20),
-              container_3
-            ],
+            children: <Widget>[container, square_1, container_3],
           ),
         ),
       ),
@@ -29,22 +25,34 @@ class MyApp extends StatelessWidget {
 }
 
 var container = Container(
-  height: 100.00,
-//  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+  width: 100,
+  margin: EdgeInsets.symmetric(vertical: 20),
 //  padding: EdgeInsets.all(20),
-  color: Colors.orangeAccent,
+  color: Colors.red,
   child: Text('Hello'),
 );
 
-var container_2 = Container(
-  height: 100.00,
-  padding: EdgeInsets.all(20),
-  color: Colors.blueGrey,
-  child: Text('Hello 2'),
+var square_1 = Container(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+          color: Colors.yellow,
+          height: 100,
+          width: 100,
+          child: Text('Hello 2')),
+      Container(
+          color: Colors.yellowAccent,
+          height: 100,
+          width: 100,
+          child: Text('Hello 2'))
+    ],
+  ),
 );
 
 var container_3 = Container(
-  height: 100.00,
-  color: Colors.red,
+  margin: EdgeInsets.symmetric(vertical: 20),
+  width: 100.00,
+  color: Colors.blue,
   child: Text('Hello 3'),
 );
