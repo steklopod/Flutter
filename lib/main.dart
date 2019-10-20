ba57f792d413e2397520078ef1ce793b75adf3aa
Dmitry Koltovich <steklopod@gmail.com>
2019-10-20 15:13:21 +0300

    02: icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
 //            mainAxisSize: MainAxisSize.max,
 //            verticalDirection: VerticalDirection.up,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[container, square_1, container_3],
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              avatar,
+              myName,
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              myProffesion,
+              myContacts,
+              myEmail
+            ],
           ),
         ),
       ),
@@ -24,35 +37,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-var container = Container(
-  width: 100,
-  margin: EdgeInsets.symmetric(vertical: 20),
-//  padding: EdgeInsets.all(20),
-  color: Colors.red,
-  child: Text('Hello'),
+var avatar = CircleAvatar(
+  backgroundImage: AssetImage('images/me.jpeg'),
+  radius: 50,
 );
 
-var square_1 = Container(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Container(
-          color: Colors.yellow,
-          height: 100,
-          width: 100,
-          child: Text('Hello 2')),
-      Container(
-          color: Colors.yellowAccent,
-          height: 100,
-          width: 100,
-          child: Text('Hello 2'))
-    ],
+var myName = Text(
+  'Дима Колтович',
+  style: TextStyle(
+    fontFamily: 'Lobster',
+    fontSize: 40,
+    color: Colors.white,
   ),
 );
 
-var container_3 = Container(
-  margin: EdgeInsets.symmetric(vertical: 20),
-  width: 100.00,
-  color: Colors.blue,
-  child: Text('Hello 3'),
+var myProffesion = Text(
+  'повелитель сусликов',
+  style: TextStyle(
+      fontFamily: 'Open Sans Condensed',
+      fontSize: 35,
+      color: Colors.teal[100],
+      letterSpacing: 2.5),
+);
+
+var myContacts = Card(
+  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+  child: ListTile(
+      leading: Icon(
+        Icons.phone,
+        color: Colors.teal,
+      ),
+      title: Text(
+        '+7(916)483-18-17',
+        style: TextStyle(
+          color: Colors.teal.shade900,
+          fontFamily: 'Open Sans Condensed',
+          fontSize: 20,
+        ),
+      )),
+);
+
+var myEmail = Card(
+  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+  child: ListTile(
+      leading: Icon(
+        Icons.email,
+        color: Colors.teal,
+      ),
+      title: Text(
+        'steklopod@gmail.com',
+        style: TextStyle(
+          color: Colors.teal.shade900,
+          fontFamily: 'Open Sans Condensed',
+          fontSize: 20,
+        ),
+      )),
 );
